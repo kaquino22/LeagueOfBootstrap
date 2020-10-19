93ec203203880aeb4b78b9cadc97b5d7fba03ed7
Kauã Aquino <kaquino@tecnologiaunica.local>
2020-10-19 11:23:09 -0300

    Botão seu audio com audio

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+$(document).ready(function() {
+
+    $("#ComAudio").click(function() {
+        $("#ComAudio").hide();
+        $("#SemAudio").show();
+        playAudio();
+    });
+
+    $("#SemAudio").click(function() {
+        $("#SemAudio").hide();
+        $("#ComAudio").show();
+        pauseAudio();
+    });
 
 
+    var x = document.getElementById("musicaLux");
+
+    function playAudio() {
+        x.play();
+    }
+
+    function pauseAudio() {
+        x.pause();
+    }
+});
